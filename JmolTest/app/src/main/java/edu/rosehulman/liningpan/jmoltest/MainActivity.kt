be363aed7be0566943d94bf05ee386c64c85ba79
Lining Pan <panl1@rose-hulman.edu>
2019-07-13 13:58:11 +0800

    Present molecule using Jsmol and Webview

---
+++ JmolTest/app/src/main/java/edu/rosehulman/liningpan/jmoltest/MainActivity.kt
@@ -0,0 +1,18 @@
+package edu.rosehulman.liningpan.jmoltest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        webView.settings.javaScriptEnabled = true
+        webView.settings.allowFileAccessFromFileURLs = true
+        webView.settings.allowUniversalAccessFromFileURLs = true
+        webView.loadUrl("file:///android_asset/jsmol/simple.htm")
+    }
+}
